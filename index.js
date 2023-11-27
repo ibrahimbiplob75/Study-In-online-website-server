f44ec72bda1c904fc2b2a11aa413ccbb26174f29
ibrahim75 <ibrahimbiplob75@gmail.com>
2023-11-27 11:43:12 +0600

    Complete

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
   origin:[
+    
+    "https://online-group-study-6a999.web.app",
     "http://localhost:5173"
+    
   ],
   credentials:true,
 }));
@@ -50,7 +53,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const database=client.db("groupStudyDB").collection("assignments");
     const userDB=client.db("userDB").collection("users");
